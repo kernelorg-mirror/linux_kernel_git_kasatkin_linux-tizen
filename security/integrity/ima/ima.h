da25ed120835f0be2b78e30a29577144e1cebca3
Dmitry Kasatkin <d.kasatkin@samsung.com>
2013-11-18 21:28:05 +0200

    ima: directory integrity protection implementation
    This patch provides implmentation of directory integrity protection hooks.
    
    This patch implements ima_dir_check() and ima_dir_update() hooks.
    
    ima_dir_check() verifies the directory integrity during the initial path
    lookup, when the dentry is just being created and may block. It allocates
    the needed data structures and performs the integrity verification.
    The results of which are cached. Subsequent calls mostly happen under
    RCU locking, when the code may not block, and returns immediately with
    the cached verification status. So ima_dir_check() does not interrupt
    RCU path walk.
    
    Directory hash is a hash over the list of directory entries, that includes
    name, ino and d_type. ima_dir_check() caclculates a directory hash and compaires
    it against good reference value stored in 'security.ima' extended attribute.
    
    ima_dir_update() is called when directory content is changing, and updates
    the directory hash.
    
    Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>

--- security/integrity/ima/ima.h
+++ security/integrity/ima/ima.h
@@ -163,7 +163,8 @@ struct integrity_iint_cache *integrity_iint_insert(struct inode *inode);
 struct integrity_iint_cache *integrity_iint_find(struct inode *inode);
 
 /* IMA policy related functions */
-enum ima_hooks { FILE_CHECK = 1, MMAP_CHECK, BPRM_CHECK, MODULE_CHECK, POST_SETATTR };
+enum ima_hooks { FILE_CHECK = 1, MMAP_CHECK, BPRM_CHECK, MODULE_CHECK,
+		 POST_SETATTR, DIR_CHECK };
 
 int ima_match_policy(struct dentry *dentry, enum ima_hooks func, int mask,
 		     int flags);
