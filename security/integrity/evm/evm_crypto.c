963a1427b50ce2892810e7d8ff6258a163656a96
Dmitry Kasatkin <d.kasatkin@samsung.com>
2014-01-17 14:47:57 +0200

    evm: do not use inode generation for special files
    There is no way to read inode generation number for special files.
    It makes it impossible to perform filesystem labeling with digital
    or HMAC signatures. Disable it for time being.
    
    Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>

--- security/integrity/evm/evm_crypto.c
+++ security/integrity/evm/evm_crypto.c
@@ -105,7 +105,11 @@ static void hmac_add_misc(struct shash_desc *desc, struct inode *inode,
 
 	memset(&hmac_misc, 0, sizeof(hmac_misc));
 	hmac_misc.ino = inode->i_ino;
-	hmac_misc.generation = inode->i_generation;
+	if (S_ISREG(inode->i_mode) || S_ISDIR(inode->i_mode))
+		hmac_misc.generation = inode->i_generation;
+	else
+		/* there is no way to read generation from user space */
+		hmac_misc.generation = 0;
 	hmac_misc.uid = from_kuid(&init_user_ns, inode->i_uid);
 	hmac_misc.gid = from_kgid(&init_user_ns, inode->i_gid);
 	hmac_misc.mode = inode->i_mode;
